eda276f1556e0ea9155490cd75c1e969a7a988d0
Michał Balicki <stiekerosiem@gmail.com>
2024-03-22 13:38:11 +0100

    fix tests

--- src/models/settlements/settlements.controller.spec.ts
+++ src/models/settlements/settlements.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { SettlementsController } from './settlements.controller';
+import { SettlementsService } from './settlements.service';
+
+const mockSettlementsService = {};
 
 describe('SettlementsController', () => {
   let controller: SettlementsController;
@@ -8,6 +11,12 @@ describe('SettlementsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SettlementsController],
+      providers: [
+        {
+          provide: SettlementsService,
+          useValue: mockSettlementsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<SettlementsController>(SettlementsController);
