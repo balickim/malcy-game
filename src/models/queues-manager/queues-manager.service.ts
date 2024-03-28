aaf9685d14caeab162cc4849a2f44acc03d8451e
Michał Balicki <stiekerosiem@gmail.com>
2024-03-28 19:03:25 +0100

    wip

--- src/models/queues-manager/queues-manager.service.ts
+++ src/models/queues-manager/queues-manager.service.ts
@@ -1,11 +1,42 @@
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as Bull from 'bull';
+import Redis from 'ioredis';
 
 @Injectable()
 export class QueuesManagerService {
   private queues: Map<string, Bull.Queue> = new Map();
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    @InjectRedis() private readonly redis: Redis,
+  ) {}
+
+  // TODO assign processors to all active or waiting jobs after server restart
+  // async getAllSettlementRecruitmentQueueNames(): Promise<string[]> {
+  //   const prefix = 'bull:settlement_';
+  //   const queueNames = new Set<string>();
+  //   let cursor = '0';
+  //
+  //   do {
+  //     const reply = await this.redis.scan(
+  //       cursor,
+  //       'MATCH',
+  //       `${prefix}*`,
+  //       'COUNT',
+  //       '1000',
+  //     );
+  //     cursor = reply[0];
+  //     reply[1].forEach((key: string) => {
+  //       const match = key.match(/^(bull:[^:]+)/);
+  //       if (match && match[1]) {
+  //         queueNames.add(match[1].replace('bull:', ''));
+  //       }
+  //     });
+  //   } while (cursor !== '0');
+  //
+  //   return Array.from(queueNames);
+  // }
 
   async generateQueue(
     name: string,
@@ -23,16 +54,12 @@ export class QueuesManagerService {
     return queue;
   }
 
-  async getAllJobsFromQueue(
-    name: string,
-    callback?: (job: Bull.Job<any>, done: Bull.DoneCallback) => Promise<void>,
-  ): Promise<Bull.Job[]> {
+  async getAllJobsFromQueue(name: string): Promise<Bull.Job[]> {
     const jobStatuses: Bull.JobStatus[] = ['active'];
     const queue: Bull.Queue = new Bull(
       name,
       this.configService.get<string>('REDIS_CONNECTION_STRING'),
     );
-    callback && queue.process(1, callback);
     return await queue.getJobs(jobStatuses);
   }
 }
--- src/models/recruit/recruit.service.ts
+++ src/models/recruit/recruit.service.ts
@@ -1,8 +1,11 @@
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as Bull from 'bull';
+import Redis from 'ioredis';
 import { Repository } from 'typeorm';
 
+import { sleep } from '~/common/utils';
 import { ArmyEntity } from '~/models/armies/entities/armies.entity';
 import { QueuesManagerService } from '~/models/queues-manager/queues-manager.service';
 import RecruitDto from '~/models/recruit/dtos/recruit.dto';
@@ -15,7 +18,19 @@ export class RecruitService {
     @InjectRepository(ArmyEntity)
     private armyRepository: Repository<ArmyEntity>,
     private queueService: QueuesManagerService,
+    @InjectRedis() private readonly redis: Redis,
   ) {}
+
+  // TODO assign processors to all active or waiting jobs after server restart
+  // bull does not keep processors code in between restarts
+  // async onModuleInit() {
+  //   const queueNames =
+  //     await this.queueService.getAllSettlementRecruitmentQueueNames();
+  //
+  //   for (const queueName of queueNames) {
+  //     await this.queueService.generateQueue(queueName, this.recruitProcessor());
+  //   }
+  // }
 
   async startRecruitment(recruitDto: RecruitDto) {
     const queueName = `settlement_${recruitDto.settlementId}`;
@@ -31,17 +46,40 @@ export class RecruitService {
   }
 
   recruitProcessor() {
-    return async (job: Bull.Job<any>, done) => {
-      for (let i = 0; i < job.data.unitCount; i++) {
-        await new Promise((resolve) => setTimeout(resolve, 60000));
-        await this.recruitUnit(job.data);
-        await job.progress(i + 1);
+    return async (job: Bull.Job<RecruitDto>, done: Bull.DoneCallback) => {
+      const totalUnits = job.data.unitCount;
+      const jobId = job.id;
+
+      for (let i = 0; i < totalUnits; i++) {
+        const currentProgress = await this.getRecruitmentProgress(
+          job.data,
+          jobId,
+        );
+        if (currentProgress < totalUnits) {
+          await sleep(5000);
+          await this.recruitUnit(job.data, jobId);
+          await job.progress(currentProgress + 1);
+        } else {
+          break;
+        }
       }
       done();
     };
   }
 
-  async recruitUnit(recruitDto: RecruitDto): Promise<void> {
+  async recruitUnit(
+    recruitDto: RecruitDto,
+    jobId: Bull.JobId,
+  ): Promise<boolean> {
+    const currentProgress = await this.getRecruitmentProgress(
+      recruitDto,
+      jobId,
+    );
+
+    if (currentProgress >= recruitDto.unitCount) {
+      return false;
+    }
+
     const army = await this.armyRepository.findOne({
       where: { settlementId: recruitDto.settlementId },
     });
@@ -52,12 +90,14 @@ export class RecruitService {
 
     army[recruitDto.unitType] += 1;
     await this.armyRepository.save(army);
+
+    await this.saveRecruitmentProgress(recruitDto, jobId, currentProgress + 1);
+    return true;
   }
 
   async getUnfinishedJobsBySettlementId(settlementId: string) {
     const jobs = await this.queueService.getAllJobsFromQueue(
       `settlement_${settlementId}`,
-      this.recruitProcessor(),
     );
 
     if (!jobs) {
@@ -66,5 +106,23 @@ export class RecruitService {
     }
 
     return jobs;
+  }
+
+  async saveRecruitmentProgress(
+    recruitDto: RecruitDto,
+    jobId: number | string,
+    progress: number,
+  ): Promise<void> {
+    const key = `recruitment:${recruitDto.settlementId}:${recruitDto.unitType}:${jobId}`;
+    await this.redis.set(key, progress.toString(), 'EX', 60 * 60 * 24 * 7); // Expire after a week
+  }
+
+  async getRecruitmentProgress(
+    recruitDto: RecruitDto,
+    jobId: number | string,
+  ): Promise<number> {
+    const key = `recruitment:${recruitDto.settlementId}:${recruitDto.unitType}:${jobId}`;
+    const progress = await this.redis.get(key);
+    return progress ? parseInt(progress, 10) : 0;
   }
 }
